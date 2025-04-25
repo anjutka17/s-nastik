f0b790fbaa8b3b9875ee9cacb31419b8f30f004c
opilane <opilane@HP-CZC2349HT9>
2025-04-25 11:37:43 +0300

    sõnastik

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 ﻿import sõnastik
 
+fail = "sõnastik.txt"
+sõnad = sõnastik.loe_failist(fail)
+
 def kuva_menuu():
-    print("\nTere tulemast eesti-vene sõnastikku!")
+    print("\n--- Eesti-Vene sõnastik ---")
     print("1 - Tõlgi eesti -> vene")
     print("2 - Tõlgi vene -> eesti")
     print("3 - Lisa uus sõna")
     print("4 - Paranda sõna")
     print("5 - Testi teadmisi")
-    print("6 - Kuula sõna (Text-to-Speech)")
-    print("7 - Välju")
+    print("6 - Välju")
 
-    while True:
-        kuva_menuu()
-        valik = input("Tee oma valik: ").strip()
-        if valik == '1':
-            sõnastik.tolgi_est_rus()
-        elif valik == '2':
-            sõnastik.tolgi_rus_est()
-        elif valik == '3':
-            sõnastik.lisa_sona()
-        elif valik == '4':
-            sõnastik.paranda_sona()
-        elif valik == '5':
-            sõnastik.testi_teadmisi()
-        elif valik == '6':
-            sõnastik.text_to_speech()
-        elif valik == '7':
-            print("Head aega!")
-            break
-        else:
-            print("Tundmatu valik!")
+while True:
+    kuva_menuu()
+    valik = input("Vali tegevus (1-6): ")
+    if valik == "1":
+        sõnastik.tolgi_est_rus(sõnad)
+    elif valik == "2":
+        sõnastik.tolgi_rus_est(sõnad)
+    elif valik == "3":
+        sõnastik.lisa_sona(sõnad)
+        sõnastik.kirjuta_failisse(fail, sõnad)
+    elif valik == "4":
+        sõnastik.paranda_sona(sõnad)
+        sõnastik.kirjuta_failisse(fail, sõnad)
+    elif valik == "5":
+        sõnastik.testi_teadmisi(sõnad)
+    elif valik == "6":
+        print("Head aega!")
+        break
+    else:
+        print("Vale valik!")
+
+# чтобы окно не закрылось сразу
+input("\nVajuta Enter, et väljuda...")
--- sõnastik.py
+++ sõnastik.py
@@ -1,76 +1,67 @@
-﻿import random
+﻿def loe_failist(failinimi):
+    sõnastik = {}
+    f = open(failinimi, "r", encoding="utf-8")
+    for rida in f:
+        rida = rida.strip()
+        if ":" in rida:
+            est, rus = rida.split(":")
+            sõnastik[est] = rus
+    f.close()
+    return sõnastik
 
-# eesti-vene sõnastik
-sõnastik = {
-'koer': 'собака',
-'kass': 'кошка',
-'maja': 'дом',
-'auto': 'машина',
-'päike': 'солнце'
-}
+def kirjuta_failisse(failinimi, sõnastik):
+    f = open(failinimi, "w", encoding="utf-8")
+    for est in sõnastik:
+        f.write(est + ":" + sõnastik[est] + "\n")
+    f.close()
 
-def tolgi_est_rus():
-	sõna = input("Sisesta sõna eesti keeles: ").lower()
-	if sõna in sõnastik:
-		print("Vene keeles:", sõnastik[sõna])
-	else:
-		print("Sõna ei leitud!")
+def tolgi_est_rus(sõnastik):
+    sõna = input("Sisesta eesti sõna: ")
+    if sõna in sõnastik:
+        print("Vene keeles:", sõnastik[sõna])
+    else:
+        print("Sõna ei leitud!")
 
-def tolgi_rus_est():
-	sõna = input("Sisesta sõna vene keeles: ").lower()
-	found = False
-	for est, rus in sõnastik.items():
-		if rus == sõna:
-			print("Eesti keeles:", est)
-			found = True
-		break
-	if not found:
-			print("Sõna ei leitud!")
+def tolgi_rus_est(sõnastik):
+    sõna = input("Sisesta vene sõna: ")
+    leitud = False
+    for est in sõnastik:
+        if sõnastik[est] == sõna:
+            print("Eesti keeles:", est)
+            leitud = True
+    if not leitud:
+        print("Sõna ei leitud!")
 
-def lisa_sona():
-	est = input("Sisesta uus sõna eesti keeles: ").lower()
-	rus = input("Sisesta selle sõna vene tõlge: ").lower()
-	if est in sõnastik:
-		print("Sõna on juba olemas!")
-	else:
-		sõnastik[est] = rus
-		print("Sõna lisatud!")
+def lisa_sona(sõnastik):
+    est = input("Sisesta uus eesti sõna: ")
+    rus = input("Sisesta selle tõlge vene keeles: ")
+    if est in sõnastik:
+        print("See sõna on juba olemas!")
+    else:
+        sõnastik[est] = rus
+        print("Sõna lisatud!")
 
-def paranda_sona():
-	vana = input("Sisesta parandatav eesti sõna: ").lower()
-	if vana in sõnastik:
-		uus_est = input("Sisesta uus eesti sõna: ").lower()
-		uus_rus = input("Sisesta uus vene tõlge: ").lower()
-		del sõnastik[vana]
-		sõnastik[uus_est] = uus_rus
-		print("Sõna parandatud!")
-	else:
-		print("Sõna ei leitud!")
+def paranda_sona(sõnastik):
+    est = input("Millist eesti sõna tahad parandada? ")
+    if est in sõnastik:
+        uus_est = input("Uus eesti sõna: ")
+        uus_rus = input("Uus vene sõna: ")
+        del sõnastik[est]
+        sõnastik[uus_est] = uus_rus
+        print("Sõna parandatud!")
+    else:
+        print("Sõna ei leitud!")
 
-def testi_teadmisi():
-	suund = input("Vali suund (1 = eesti → vene, 2 = vene → eesti): ")
-	punktid = 0
-	kokku = 3
-	sõnad = list(sõnastik.items())
-	random.shuffle(sõnad)
-
-	for i in range(kokku):
-		est, rus = sõnad[i]
-		if suund == '1':
-			vastus = input(f"Sõna '{est}' vene keeles: ").lower()
-			if vastus == rus:
-				print("Õige!")
-				punktid += 1
-			else:
-				print(f"Vale! Õige vastus: {rus}")
-		elif suund == '2':
-			vastus = input(f"Sõna '{rus}' eesti keeles: ").lower()
-			if vastus == est:
-				print("Õige!")
-				punktid += 1
-			else:
-				print(f"Vale! Õige vastus: {est}")
-				protsent = punktid / kokku * 100
-				print(f"Sinu tulemus: {protsent:.0f}%")
-	
-
+def testi_teadmisi(sõnastik):
+    punktid = 0
+    kokku = 3
+    loetelu = list(sõnastik.items())
+    for i in range(min(kokku, len(loetelu))):
+        est, rus = loetelu[i]
+        vastus = input("Mis on vene keeles: " + est + "? ")
+        if vastus == rus:
+            print("Õige!")
+            punktid += 1
+        else:
+            print("Vale. Õige vastus oli:", rus)
+        print("Said", punktid, "punkti", "/", kokku)
